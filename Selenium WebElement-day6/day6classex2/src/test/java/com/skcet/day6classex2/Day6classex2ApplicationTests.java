70927f71315a7134a628eebe1df4214dfd8da18f
kaviya jothi <124349280+kaviya1144@users.noreply.github.com>
2024-04-22 16:24:35 +0530

    Add files via upload

---
+++ Selenium WebElement-day6/day6classex2/src/test/java/com/skcet/day6classex2/Day6classex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skcet.day6classex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day6classex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Selenium WebElement-day6/day6classexercise1/src/test/java/day6/portal/day6classexercise1/Day6classexercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package day6.portal.day6classexercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day6classexercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
